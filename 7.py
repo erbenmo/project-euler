5ab403eae2ebc17a4badf59c9c2a813b6b03f573
Erben Mo <erbenmo@gmail.com>
2013-04-04 22:41:27 -0700

    7

--- 7.py
+++ 7.py
@@ -1,15 +1,22 @@
-from math import sqrt, floor
+from math import sqrt, floor, ceil
 
 
 def isPrime(n):
+    if n == 2:
+        return True
+    
     for i in range(2, int(floor(sqrt(n)))+1):
         if n % i == 0:
             return False
     return True
 
-i == 0
+i = 1
 num = 2
-while i != 10001:
+while True:
     if isPrime(num):
+        print num
+        if i == 10001:
+            print num
+            break
         i += 1
     num += 1
