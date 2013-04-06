1484c324f23bee8c5e5b2d727ace367e36a51cf2
Erben Mo <erbenmo@gmail.com>
2013-04-06 16:20:47 -0700

    18 & 67

---
+++ 67.py
@@ -0,0 +1,12 @@
+f = open("67")
+
+input = f.readlines()
+input = map(lambda x:map(int, x.split()), input)
+
+i = len(input)-2
+while i>=0:
+    for j in range(0, i+1):
+        input[i][j] += max(input[i+1][j], input[i+1][j+1])
+    i -= 1
+
+print input[0][0]
